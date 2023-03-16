9a17679d719c89603d04b299101531e34af7861d
rustana <rustanya82@gmail.com>
2023-03-16 14:38:29 -0400

    frame width updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
+import ListToDo from "./components/ListToDo";
+import Header from "./components/Header";
+import {nanoid} from "nanoid";
+import {useState} from "react";
+import CreateTask from "./components/CreateTask";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const taskList = [
+        {
+            id: nanoid(),
+            name: "Learn React",
+            done: false
+        },
+        {
+            id: nanoid(),
+            name: "Learn css",
+            done: true
+        },
+        {
+            id: nanoid(),
+            name: "Learn TypeScript",
+            done: false
+        }]
+    const [list, setList] = useState(taskList)
+    const changeStatus = (id) => {
+        const updatedTask = list.map(el => el.id === id ? {...el, done: !el.done} : el)
+        setList(updatedTask)
+    }
+    const deleteTask = (id) => {
+        setList(list.filter(el => el.id !== id))
+    }
+    const createTask = (newTask) => {
+        setList([...list, newTask])
+    }
+    const updateTask = (id, newName) => {
+        const updatedList=list.map(el=>el.id===id?{...el,name:newName}:el)
+        setList(updatedList)
+    }
+    return (
+
+            <div className='frame'>
+            <div className='container'>
+            <Header/>
+            <CreateTask createTask={createTask}/>
+            <ListToDo list={list}
+                      changeStatus={changeStatus}
+                      deleteTask={deleteTask}
+                      createTask={createTask}
+                      updateTask={updateTask}
+            />
+            </div>
+            </div>
+    )
 }
 
 export default App;
